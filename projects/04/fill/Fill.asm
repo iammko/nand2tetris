1c4eeca5818ed6d24e76f24abe9d65adafba6e04
iammko <iammko@163.com>
2020-11-13 11:55:18 +0800

    04 homework fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,72 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@status         // init status
+M=0
+D=0
+@i              // init start screen address idx
+M=0
+
+(SETLOOP)
+@KBD
+D=M
+@SETLOOP2   
+D;JEQ
+D=-1        // confirm status
+
+(SETLOOP2)
+@TEMP       // save new status
+M=D
+@status
+D=D-M
+@SETIDX     // no change, keep set next address
+D;JEQ
+@TEMP
+D=M
+@status     // new status start by cur idx
+M=D
+
+(SETADDRESS)
+@i
+D=M
+@SCREEN
+D=D+A
+@address
+M=D
+@status
+D=M
+@address
+A=M             // set A to SCREEN[idx]
+M=D             // SCREEN[idx]=status
+@SETLOOP
+0;JMP
+
+(SETNOKEYIDX)
+@i
+D=M             //no key, if i <= 0 then stop
+@SETLOOP
+D;JLE
+@i
+M=M-1           // set point to SCREEN[idx-1]
+@SETADDRESS
+0;JMP
+
+(SETIDX)
+@TEMP
+D=M
+@SETNOKEYIDX
+D;JEQ           //nokey
+@i              // has key, if i >= 8192 then keep
+D=M
+@8192
+D=D-A
+@SETLOOP
+D;JGE
+@i
+M=M+1           // set point to SCREEN[idx+1]
+@SETADDRESS
+0;JMP
+
+
+
