2e5eee16af0857f403f1465ea03f4c7729b3422d
Muuko <iammko@163.com>
2021-04-19 02:12:45 +0800

    10 analyzer

---
+++ projects/10/JackAnalyzer.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+if len(sys.argv) < 2:
+    sys.exit(" use python %s : file | path "%sys.argv[0])
+
+source = sys.argv[1]
+
+def __analyzeFile(file):
+    pass
+
+def __analyzeDir(dir):
+    pass
+
+
+def __analyze(source_param):
+
+    if os.path.isfile(source_param):
+        __analyzeFile(source_param)
+        return
+
+    if os.path.isdir(source_param):
+        __analyzeDir(source_param)
+        return
+
+
+__analyze(source)
---
+++ projects/10/JackTokenizer.py
@@ -0,0 +1,9 @@
+class JackTokenizer:
+    def __init__(self, file):
+        self.fd = open(file, 'w')
+
+    def hasMoreTokens(self):
+
+        
+
+        return True
