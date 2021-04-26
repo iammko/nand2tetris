3429cbaf30ccacfb537246e980e637604158b9fb
Muuko <iammko@163.com>
2021-04-26 11:03:35 +0800

    enum

--- projects/10/JackTokenizer.py
+++ projects/10/JackTokenizer.py
@@ -1,6 +1,10 @@
 class JackTokenizer:
     def __init__(self, file):
         self.fd = open(file, 'w')
+        self.cur_token = None
+        self.next_token = None
+
+        
 
     def hasMoreTokens(self):
 
