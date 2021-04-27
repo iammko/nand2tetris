afce9dab0324db841b48c1b5aab6e23549eac672
iammko <iammko@163.com>
2021-04-27 21:27:49 +0800

    flag

--- projects/10/JackTokenizer.py
+++ projects/10/JackTokenizer.py
@@ -3,11 +3,25 @@ class JackTokenizer:
         self.fd = open(file, 'w')
         self.cur_token = None
         self.next_token = None
-
+        self.cur_line = ''
+        # 是否在双引号字符串当中
+        self.inString = False
+        # 是否在注释块当中
+        self.inBlock = False
         
+        self.symbol = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<', '>', '=', '~']
+    
+
+    def parseToken(self):
+
+        pass
 
     def hasMoreTokens(self):
 
+
+
+
+    
         
 
         return True
